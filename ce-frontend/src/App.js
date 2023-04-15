c0fb068fc03f7cb460d6bcf1a8e7c05374a0b576
k12126565 <k12126565@students.jku.at>
2023-04-15 15:31:30 +0200

    TestCommit

--- ce-frontend/src/App.js
+++ ce-frontend/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+//problem with openssl on windows local problem(M.Sch)
 function App() {
   return (
-    <div className="App">
+      <div className="App">
      <p>Trading bot</p>
     </div>
   );
